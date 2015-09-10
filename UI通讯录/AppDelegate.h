a3766c09d30ed7a50aee10d4651b937e453e12e0
xingjiahe <564710948@qq.com>
2015-09-10 19:15:59 +0800

    Initial Commit

---
+++ UI通讯录/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UI通讯录
+//
+//  Created by 邢家赫 on 15/9/10.
+//  Copyright (c) 2015年 邢家赫. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UI通讯录/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  UI通讯录
+//
+//  Created by 邢家赫 on 15/9/10.
+//  Copyright (c) 2015年 邢家赫. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
